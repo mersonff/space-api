1a1ae0bf40c1ebc44bc6964d596d9dd31e518faa
Emerson Freitas <mersonsi@gmail.com>
2022-12-23 15:02:24 -0300

    feat: ajustes nos testes do service e criação da task

--- spec/services/consuming_api_service_spec.rb
+++ spec/services/consuming_api_service_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe ConsumingApiService do
   context 'when #persist_data' do
-    let (:persist) { ConsumingApiService.new(10).persist_data }
+    let(:persist) { described_class.new(10).persist_data }
 
     it 'creates 10 articles' do
-      expect { persist }.to change { Article.count }.by(10)
+      expect { persist }.to change(Article, :count).by(10)
     end
   end
 end
