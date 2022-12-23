81867a4e8d393542934e0a347ad29fc0d9a8ffdd
Emerson Freitas <mersonsi@gmail.com>
2022-12-23 17:41:13 -0300

    feat: adiciona rota padrão

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  root to: 'v1/home#index'
 
   namespace :v1, defaults: { format: :json } do
-    get 'home' => 'home#index'
     resources :articles
   end
 end
